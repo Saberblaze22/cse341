f2da384afa3a36a89bf5a436eaf3dbc3627a282d
Saberblaze22 <Comiccooldylan@gmail.com>
2025-03-18 18:58:52 -0600

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const router =require('express').Router();
 
-router.use('/', require('./swagger'));
-
 router.get('/', (req, res) => {
     //#swagger.tags=['hello world']
     res.send('hello people');});
 
 router.use('/users', require('./users'));
+router.use('/api-docs', require('./swagger'));
 
 module.exports = router;
