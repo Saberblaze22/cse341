940b4a1efd4f071a8a13739385783b9aba9aab92
Saberblaze22 <Comiccooldylan@gmail.com>
2025-03-27 12:55:35 -0600

    commit

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,33 +3,33 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllContacts = async (req, res) =>{
     //#swagger.tags=['Contacts]
-    const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users)=>{
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    result.toArray().then((contacts)=>{
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users);
+        res.status(200).json(contacts);
     });
 };
 
 const getSingleContacts = async(req, res) =>{
     //#swagger.tags=['Contacts]
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('users').find({_id: userId });
-    result.toArray().then((users)=>{
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId });
+    result.toArray().then((comtacts)=>{
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
+        res.status(200).json(contacts[0]);
     });
 };
 
 const createContacts=async(req, res) =>{
     //#swagger.tags=['Contacts]
-    const user={
+    const contact={
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response=await mongodb.getDatabase().db().collection('users').insertOne(user);
+    const response=await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
     if (response.acknowledged){
         res.status(204).send()
     }else{
@@ -39,15 +39,15 @@ const createContacts=async(req, res) =>{
 
 const updateContacts=async(req, res) =>{
     //#swagger.tags=['Contacts]
-    const userId = new ObjectId(req.params.id);
-    const user={
+    const contactId = new ObjectId(req.params.id);
+    const contact={
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response=await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId }, user);
+    const response=await mongodb.getDatabase().db().collection('contacts').replaceOne({_id: contactId }, contact);
     if (response.modifiedCount > 0){
         res.status(204).send()
     }else{
@@ -57,8 +57,8 @@ const updateContacts=async(req, res) =>{
 
 const deleteContacts=async(req, res) =>{
     //#swagger.tags=['Contacts]
-    const userId = new ObjectId(req.params.id);
-    const response=await mongodb.getDatabase().db().collection('users').deleteOne({_id: userId });
+    const contactId = new ObjectId(req.params.id);
+    const response=await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: contactId });
     if (response.deletedCount > 0){
         res.status(204).send()
     }else{
