39a0f61494c0b4af3eeecd9943f79d7a6ea94d47
Saberblaze22 <Comiccooldylan@gmail.com>
2025-04-05 18:49:52 -0600

    commit

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser=require('body-parser');
+const dotenv = require('dotenv').config();
 const mongodb =require('./data/database');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github2').Strategy;
 const app = express();
 
 const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -15,7 +26,36 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Methods', 'Get, Post, Put, Delete, Options');
     next();
 });
-app.use('/', require('./routes'));
+app.use(cors({ methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] }));
+app.use(cors({ origin: '*' }));
+app.use('/', require('./routes/index.js'));
+
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
+},
+function (accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+app.get('/', (req, res) => {res.send(req.session.user !==undefined ? `logged in as ${req.session.user.displayname}` : 'logged out')});
+app.get('/auth/github', passport.authenticate('github', {
+    failureRedirect: '/api-docs',session: false}),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/');
+    });
+
+    
 mongodb.initDb((err)=>{
     if (err){
         console.log(err);
